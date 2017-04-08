984998a30cadbb49aa4f88c69ca7aed48771d1ea
Bradley <irwinbrad2001@icloud.com>
2017-04-08 13:02:37 -0600

    Initial Commit

---
+++ SaveTheKittens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheKittens
+//
+//  Created by JIM J IRWIN on 4/8/17.
+//  Copyright © 2017 JIM J IRWIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
